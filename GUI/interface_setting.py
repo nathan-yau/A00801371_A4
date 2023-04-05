ef7ec7109368ed86b3d239f0b9bb8a9fb5f6ad42
nathan-yau <nshyau@gmail.com>
2023-04-04 22:58:37 -0700

    Add interface_setting.py

---
+++ GUI/interface_setting.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from functools import partial
+from save_load.load_game_file import load_file
+
+
+def gui_default_setting(game_window, game_title, icon_path, window_size):
+    icon = tk.PhotoImage(file=icon_path)
+    game_window.grid_rowconfigure(0, weight=1)
+    game_window.grid_columnconfigure(0, weight=1)
+    game_window.resizable(False, False)
+    game_window.title(game_title)
+    game_window.iconphoto(False, icon)
+    game_window.geometry(window_size)
+    game_window.protocol("WM_DELETE_WINDOW", closing_event)
+
+
+def closing_event() -> None:
+    if tk.messagebox.askyesno(title="Confirm?", message="Do you confirm to close this awesome game?"):
+        quit()
+
+
+def gui_menubar(overall_gui) -> None:
+    menu_bar = tk.Menu()
+
+    def create_file_menu():
+        file_menu = tk.Menu(menu_bar, tearoff=False)
+        menu_bar.add_cascade(menu=file_menu, label="File")
+
+        file_dict = {"Load Game": partial(load_file, overall_gui),
+                     "Seperator": None, "Exit Game": closing_event}
+        for key, value in file_dict.items():
+            if key == "Seperator":
+                file_menu.add_separator()
+            else:
+                file_menu.add_command(label=key, command=value)
+
+    create_file_menu()
+    overall_gui['GUI'].config(menu=menu_bar)
