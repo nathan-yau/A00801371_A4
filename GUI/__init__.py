630433cf95c771c9b33e8ed2c08733d0eab07902
nathan-yau <nshyau@gmail.com>
2023-04-04 22:58:27 -0700

    Add GUI package

---
+++ GUI/__init__.py
@@ -0,0 +1,13 @@
+GAME_NAME = "Oasis of the Lost Adventure"
+GAME_ICON = "./images/icon.png",
+GAME_COVER_PHOTO = "./images/cover_photo.png"
+GAME_MAP_PATH = "./images/maps/oasis_map_{}.png"
+GAME_ITEM_IMAGE_PATH = "./images/{}.png"
+GAME_ENEMY_IMAGE_PATH = "./images/enemy/{}.png"
+GAME_LOCATION_IMAGE_PATH = "./images/locations/{}.png"
+GUI_WINDOW_SIZE = "820x600"
+DEFAULT_FONT = "Cascadia Code"
+DEFAULT_FONT_SIZE = 10
+DEFAULT_BUTTON_WIDTH = 10
+MAX_LEN_NAME = 10
+MIN_LEN_NAME = 0
