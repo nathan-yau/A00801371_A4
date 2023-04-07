1dfaaaaef135906cbe7ff79b11dc1da3ea54523b
NATHAN YAU <nshyau@gmail.com>
2023-04-06 20:58:41 -0700

    Update create_character()

--- new_game/new_player.py
+++ new_game/new_player.py
@@ -9,12 +9,13 @@ def create_character(name) -> dict:
     :return: a dictionary that contains the starting coordinates and health points (HP) of a character
     """
 
-    keys = ["Name", "Level", "Status", "EXP", "X-coordinate", "Y-coordinate", "Items",
+    keys = ["Name", "Level", "NEXT LV (EXP)", "Status", "X-coordinate", "Y-coordinate", "Items",
             "Current HP", "Current MP", "Max HP", "Max MP",
             "Strength", "Dexterity", "Intelligence", "Magic Power"]
-    values = [name, 1, "Healthy", 0, 4, 4, {'Sanctum Key': 1, 'Healing Potion': 2,
-                                            'Status Potion': 2, 'Attribute Potion': 2,
-                                            'Raw Pig': 1, 'Oasis Explorer': 1}, 50, 50, 50, 50]
+    values = [name, 1, int(60*(random.uniform(1.5, 1.9))), "Healthy", 4, 4, {'Sanctum Key': 1, 'Healing Potion': 2,
+                                                                          'Status Potion': 2, 'Attribute Potion': 2,
+                                                                    'The Amulet of Knowledge': 1,
+                                                                    'Oasis Explorer': 1}, 50, 50, 50, 50]
     while sum(values[11:]) < 160:
         [values.append(random.randint(30, 50)) for _ in keys]
     attributes = dict(zip(keys, values))
