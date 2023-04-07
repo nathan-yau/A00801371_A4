ac9f524e3101508bcd22950dd448d0c473f3f7e8
NATHAN YAU <nshyau@gmail.com>
2023-04-07 14:00:30 -0700

    Update docstring of gui_default_setting()

--- GUI/interface_setting.py
+++ GUI/interface_setting.py
@@ -3,7 +3,27 @@ from functools import partial
 from save_load.load_game_file import load_file
 
 
-def gui_default_setting(game_window, game_title, icon_path, window_size):
+def gui_default_setting(game_window, game_title: str, icon_path: str, window_size: str) -> None:
+    """
+    Create default window and grid setting for a tkinter GUI.
+
+    :param game_window: a tkinter root window
+    :param game_title: a string that represents the title of the game
+    :param icon_path: an image path that represents an existing image
+    :param window_size: a geometry specifier as string in the format like "100x120"
+                        where 100 represents the width and 120 represents the height of the GUI
+    :precondition: a tkinter root window must exist
+    :precondition: game_window must be the tkinter root window
+    :precondition: game_title must be a string
+    :precondition: icon_path must be an image path that represents an existing image
+    :precondition: window_size must be a geometry specifier as string in the format like "100x120"
+                   where 100 represents the width and 120 represents the height of the GUI
+    :postcondition: create default window and grid setting for a tkinter GUI
+    :raise AttributeError: if game_window is not a tkinter root window
+    :raise _tkinter.TclError: if icon_path is not an image path that represents an existing image or the image
+                              does not exist in the path it represents
+                              if window_size must be a geometry specifier as string in the format like "100x120"
+    """
     icon = tk.PhotoImage(file=icon_path)
     game_window.grid_rowconfigure(0, weight=1)
     game_window.grid_columnconfigure(0, weight=1)
@@ -11,7 +31,7 @@ def gui_default_setting(game_window, game_title, icon_path, window_size):
     game_window.title(game_title)
     game_window.iconphoto(False, icon)
     game_window.geometry(window_size)
-    game_window.protocol("WM_DELETE_WINDOW", closing_event)
+    game_window.protocol("WM_DELETE_WINDOW", partial(closing_event, game_window))
 
 
 def closing_event() -> None:
