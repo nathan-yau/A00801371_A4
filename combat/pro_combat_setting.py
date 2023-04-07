ea09bb132cfa7c2c6f36ce8cc1f61682bf7ffa4d
NATHAN YAU <nshyau@gmail.com>
2023-04-06 21:05:05 -0700

    Add level_up() and drop_item()

--- combat/pro_combat_setting.py
+++ combat/pro_combat_setting.py
@@ -1,5 +1,26 @@
 import random
 from GUI import DEFAULT_FONT
+from GUI_update.status_frame import update_status_message
+
+
+def level_up(overall_gui_frame, game_info):
+    player_info = game_info['character']
+    if player_info['NEXT LV (EXP)'] <= 0:
+        player_info['Level'] += 1
+        player_info['NEXT LV (EXP)'] = int(player_info['Level']*60*(random.uniform(1.5, 1.9)))
+        for attribute in ["Strength", "Dexterity", "Intelligence", "Magic Power"]:
+            player_info[attribute] += random.randint(5, 15)
+        player_info['Max HP'] = max(round(player_info['Strength'] + player_info['Intelligence'], -1), 60)
+        player_info['Max MP'] = max(round(player_info['Dexterity'] + player_info['Magic Power'], -1), 60)
+        player_info['Current HP'] = player_info['Max HP']
+        player_info['Current MP'] = player_info['Max MP']
+        overall_gui_frame['Status Frame'].children['character_status'].config(
+            text=update_status_message(game_info['character']))
+        overall_gui_frame['Script Frame'].children['script_display'].config(text='Level Up!')
+
+
+def drop_item(gui_widgets_all, game_info, picked_foe):
+    pass
 
 
 def battle_end_widget_update(gui_widgets_all, game_info, picked_foe):
