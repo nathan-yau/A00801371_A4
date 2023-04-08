4e28aca1bd6be56e2aa48b1ad24ce2fa624b0d33
NATHAN YAU <nshyau@gmail.com>
2023-04-08 16:14:48 -0700

    Add test_random_run_away_probability.py

---
+++ test/test_random_run_away_probability.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from unittest.mock import patch
+from combat.combat_actions import random_run_away_probability
+
+
+class RandomRunAwayProbabilityTest(TestCase):
+
+    @patch("random.randint", return_value=0)
+    def test_return_true(self, _):
+        self.assertTrue(random_run_away_probability())
+
+    @patch("random.randint", return_value=1)
+    def test_return_false_with_generated_1(self, _):
+        self.assertFalse(random_run_away_probability())
+
+    @patch("random.randint", return_value=1)
+    def test_return_false_with_generated_2(self, _):
+        self.assertFalse(random_run_away_probability())
