3e2127bad06b0b1fa8bea48c67eb61d2e170aee2
NATHAN YAU <nshyau@gmail.com>
2023-04-09 16:03:55 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,10 @@
 from GUI.homepage import create_homepage
-from playsound import playsound
 
 
 def main():
     """
     Drive the program.
     """
-    # playsound('./music/default.mp3', block=False)
     game_gui = create_homepage()
     game_gui['GUI'].mainloop()
 
