afc633dc4d96d900a09aba28c8a6da963632de7b
NATHAN YAU <nshyau@gmail.com>
2023-04-08 17:04:25 -0700

    Update docstring of update_enemy_info()

--- combat/enemy_info.py
+++ combat/enemy_info.py
@@ -43,7 +43,32 @@ def enemy_status(enemy_info: dict) -> str:
     return character_status.rstrip()
 
 
-def update_enemy_info(overall_frames_info, enemy, script):
+def update_enemy_info(overall_frames_info: dict, enemy: dict, script: str) -> None:
+    """
+    Update the enemy related widgets (image and text labels) on the GUI based on the
+    enemy's information and given script string.
+
+    :param overall_frames_info: a dictionary containing key named as ['Script Frame'],
+                                whose value is a tkinter Frame
+    :param enemy: a dictionary that contain the "Name", "EXP", and "HP" of an enemy as the first three keys
+                    in this order and their corresponding data as values
+    :param script: a string
+    :precondition: overall_frames_info must be a dictionary containing key named as ['Script Frame'],
+                   whose value is a tkinter Frame
+    :precondition: the frame represented by the key ['Script Frame'] in overall_frames_info must contain two labels,
+                   ['enemy_info'] and ['script_display']
+    :precondition: enemy must be a dictionary that contain the "Name", "EXP", and "HP" of an enemy
+                       as the first three keys and their corresponding data as values
+    :precondition: enemy must contain keys named as "Name", "EXP" and "HP" as the first three keys in this order
+    :precondition: script must be a string
+    :postcondition: update the enemy related widgets (image and text labels) on the GUI based on the
+                    enemy's information and given script string
+    :raise KeyError: if enemy does not contain keys named as "Name", "EXP", and "HP" as the first
+                     three keys in this order
+                     if frame represented by the key ['Script Frame'] in overall_frames_info does not contain labels,
+                     ['enemy_info'] and ['script_display']
+    :raise TypeError: if overall_frames_info, enemy and/or script is not a dictionary
+    """
     image_name = enemy['Name'].lower().replace(" ", "_")
     overall_frames_info['Script Frame'].children['enemy_info'].config(text=f"{enemy_status(enemy)}")
     update_image_label(overall_frames_info['Script Frame'], "image_box", GAME_ENEMY_IMAGE_PATH.format(image_name))
