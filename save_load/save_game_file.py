af2b4d6c8e1e0d71fc67776ab73441263a4a9160
NATHAN YAU <nshyau@gmail.com>
2023-04-08 17:39:52 -0700

    Update docstring of create_save_file()

--- save_load/save_game_file.py
+++ save_load/save_game_file.py
@@ -4,16 +4,20 @@ from save_load.uid_converter import encoder
 from save_load import DEFAULT_SAVE_FOLDER
 
 
-def create_save_file(save_slot, all_key_attributes):
+def create_save_file(save_slot: int, all_key_attributes: dict) -> None:
     """
     Saves an encoded dictionary as a string to a file, given a save slot and all key attributes.
 
-    :parameter save_slot: an integer that represents the slot in which the save file will be stored
+    :parameter save_slot: an integer that represents the slot in which the save file will be stored,
+                          or -1 to indicate for open file window for selection
     :parameter all_key_attributes: a dictionary that contains all player data and game environment data
     :precondition: save_slot must be greater than or equal to -1
     :precondition: all_key_attributes must be a dictionary that conatins all player data and game environment data
     :postcondition: create a plan text file, which contains an encoded dictionary as string
+    :raise TypeError: if all_key_attributes is not a dictionary
     """
+    if type(all_key_attributes) is not dict:
+        raise TypeError("all_key_attributes must be dictionary")
     if save_slot == -1:
         filename = filedialog.askopenfilename(title="Select a File", initialdir=DEFAULT_SAVE_FOLDER,
                                               filetypes=(("SAVE File", "*.save"), ("SAVE File", " ")))
