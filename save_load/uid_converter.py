9d4a6341523491bc16183cc73f66c1126e214b8e
NATHAN YAU <nshyau@gmail.com>
2023-04-08 17:43:36 -0700

    Update docstring of decoder()

--- save_load/uid_converter.py
+++ save_load/uid_converter.py
@@ -1,15 +1,23 @@
 def decoder(save_uid: str) -> str:
     """
-    Convert a set of joined Unicode numeric strings to a string.
+    Convert a string of joined Unicode numeric strings into an equivalent Unicode character string
 
-    :param save_uid: a string
-    :return: a converted string from a set of joined unicode numeric string
+    :param save_uid: a string with a length that is a multiple of four
+    :precondition: phrase must be a string that is the length of multipler of four
+    :postcondition: convert a string of joined Unicode numeric strings to a string of equivalent Unicode characters
+    :return: a converted Unicode character string from an equivalent string of joined Unicode numeric strings
+    :riase ValueError: if save_uid is not a set of Unicode numeric characters
+                        with length of a multiple of four
+
     >>> decoder("00970098")
     'ab'
     >>> decoder("00970099")
     'ac'
     """
     result = []
+    if not save_uid.isdigit() or len(save_uid) % 4 != 0:
+        raise ValueError("save_uid must be a string of Unicode numeric characters "
+                         "with a length that is a multiple of four!")
     for index in range(0, int(len(save_uid)), 4):
         result.append(chr(int(save_uid[index:index + 4])))
     return ''.join(result)
