16104476ad3e99ed784513d0d5ff2c285f0187a9
NATHAN YAU <nshyau@gmail.com>
2023-04-08 17:04:16 -0700

    Update docstring of enemy_status()

--- combat/enemy_info.py
+++ combat/enemy_info.py
@@ -2,10 +2,41 @@ from GUI import GAME_ENEMY_IMAGE_PATH
 from GUI.create_widgets import update_image_label
 
 
-def enemy_status(player_info):
+def enemy_status(enemy_info: dict) -> str:
+    """
+    Convert a dictionary that contains the "Name", "EXP", and "HP" of an enemy as keys and their corresponding
+    values into an aligned string in multiple lines  with a title of "Enemy Information".
+
+    :param enemy_info: a dictionary that contain the "Name", "EXP", and "HP" of an enemy as the first three keys
+                        in this order and their corresponding data as values
+    :precondition: enemy_info must be a dictionary that contain the "Name", "EXP", and "HP" of an enemy
+                   as the first three keys and their corresponding data as values
+    :precondition: enemy_info must contain keys named as "Name", "EXP" and "HP"
+    :precondition: "Name", "EXP" and "HP" must be the first three keys of the enemy_info dictioary in this order
+    :postcondition: convert a dictionary that contains the "Name", "EXP", and "HP" of an enemy as keys and
+                    their corresponding values into an aligned string in multiple lines  with a title of
+                    "Enemy Information"
+    :return: an aligned string in multiple lines that contains the "Name", "EXP", and "HP" of an enemy with a title of
+             "Enemy Information"
+    :raise KeyError: if enemy_info does not contain keys named as "Name", "EXP", and "HP" as the first
+                     three keys in this order
+    :raise TypeError: if enemy_info is not a dictionary
+
+    >>> character_dict = {"Name": "Nathan", "EXP": 100, "HP": 200, "MP": 404}
+    >>> enemy_status(character_dict)
+
+
+    >>> foe_dict = {"Name": "Nathan", "EXP": 100, "HP": 200}
+    >>> enemy_status(foe_dict)
+    'Enemy Information\\n\\nName        Nathan\\nEXP            100\\nHP             200'
+    """
+    if type(enemy_info) is not dict:
+        raise TypeError("enemy_info must be a dictionary")
+    if list(enemy_info.keys())[:3] != ['Name', 'EXP', 'HP']:
+        raise KeyError("the first three keys of enemy_info must be 'Name', 'EXP', and 'HP' in this order")
     character_status = "Enemy Information\n\n"
-    keys_print = set(list(player_info.keys())[:3])
-    for key, value in player_info.items():
+    keys_print = set(list(enemy_info.keys())[:3])
+    for key, value in enemy_info.items():
         if key not in keys_print:
             continue
         character_status += f"{key:<6}{value:>12}\n"
