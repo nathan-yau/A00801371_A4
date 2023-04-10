a35867370cad0d14af108ee6d336ef8ca34cf270
nathan-yau <nshyau@gmail.com>
2023-04-10 10:18:21 -0700

    Update map_widget_update()

--- GUI_update/update_map_widget.py
+++ GUI_update/update_map_widget.py
@@ -3,7 +3,7 @@ from GUI import GAME_MAP_PATH
 from GUI.create_widgets import update_image_label
 
 
-def map_widget_update(avatar: dict, overall_frames: dict, game_map_path: str = GAME_MAP_PATH) -> None:
+def map_widget_update(avatar: dict, overall_frames: dict, map_path: str = GAME_MAP_PATH) -> None:
     """
     Update the game map on the GUI based on the player's current position.
 
@@ -11,7 +11,7 @@ def map_widget_update(avatar: dict, overall_frames: dict, game_map_path: str = G
                    each of which has a value representing the current coordinate of the player in the game map.
     :param overall_frames: a dictionary containing keys named as ['Event Bar'] and ['Status Frame'],
                            which represents a tkinter Label widget and tkinter Frame widget
-    :param game_map_path: a file path referring to the game map image
+    :param map_path: a file path referring to the game map image
     :precondition: avatar must be a dictionary
     :precondition: avatar must contain keys named as ['X-coordinate'] and ['Y-coordinate']
     :precondition: the values of ['X-coordinate'] and ['Y-coordinate'] must be an integer value
@@ -20,7 +20,7 @@ def map_widget_update(avatar: dict, overall_frames: dict, game_map_path: str = G
     :precondition: the value of ['Event Bar'] must be a tkinter Label widget
     :precondition: the value of ['Status Frame'] must be a tkinter Frame widget
     :precondition: the Frame represented by ['Status Frame'] must contain a label called current_map
-    :precondition: game_map_path must be a file path referring to the game map image
+    :precondition: map_path must be a file path referring to the game map image
     :postcondition: update the game map on the GUI based on the player's current position
     :raises KeyError: if avatar does not contain keys named as ['X-coordinate'] and ['Y-coordinate']
                       if overall_frames does not contain keys named as ['Event Bar'] and ['Status Frame']
@@ -29,9 +29,9 @@ def map_widget_update(avatar: dict, overall_frames: dict, game_map_path: str = G
     :raises FileNotFoundError: if game map image cannot be found
     """
     coordinate = str(avatar["X-coordinate"])+str(avatar["Y-coordinate"])
-    if pathlib.Path(game_map_path.format(coordinate)).is_file():
+    if pathlib.Path(map_path.format(coordinate)).is_file():
         update_image_label(frame_object=overall_frames['Status Frame'], label_name="current_map",
-                           image_directory=GAME_MAP_PATH.format(coordinate))
+                           image_directory=map_path.format(coordinate))
         overall_frames['Event Bar'].config(text=f"Current Coordinate - {str(avatar['X-coordinate'])}, "
                                                 f"{str(avatar['Y-coordinate'])}")
     else:
