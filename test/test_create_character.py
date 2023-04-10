e42c75e0ed0ab09077f58a812cb3f3d82cb073ed
nathan-yau <nshyau@gmail.com>
2023-04-10 03:06:38 -0700

    Update test_create_character.py

--- test/test_create_character.py
+++ test/test_create_character.py
@@ -17,7 +17,7 @@ class CreateCharacterTest(TestCase):
     @patch("random.randint", side_effect=[30, 30, 30, 30, 30])
     def test_create_min_value_character(self, _, __):
         expected = {'Name': 'Nathan', 'Level': 1, 'NEXT LV (EXP)': 90, 'Status': 'Healthy', 'X-coordinate': 4,
-                    'Y-coordinate': 4, 'Items': {'Sanctum Key': 1}, 'Escape': False, 'Current HP': 60,
+                    'Y-coordinate': 4, 'Items': {}, 'Escape': False, 'Current HP': 60,
                     'Current MP': 60, 'Max HP': 60, 'Max MP': 60, 'Strength': 30, 'Dexterity': 30,
                     'Intelligence': 30, 'Magic Power': 30, 'Magic Resistance': 30}
         self.assertEqual(expected, create_character("Nathan"))
@@ -26,7 +26,7 @@ class CreateCharacterTest(TestCase):
     @patch("random.randint", side_effect=[50, 50, 50, 50, 50])
     def test_create_max_value_character(self, _, __):
         expected = {'Name': 'Nathan', 'Level': 1, 'NEXT LV (EXP)': 90, 'Status': 'Healthy', 'X-coordinate': 4,
-                    'Y-coordinate': 4, 'Items': {'Sanctum Key': 1}, 'Escape': False, 'Current HP': 100,
+                    'Y-coordinate': 4, 'Items': {}, 'Escape': False, 'Current HP': 100,
                     'Current MP': 100, 'Max HP': 100, 'Max MP': 100, 'Strength': 50, 'Dexterity': 50,
                     'Intelligence': 50, 'Magic Power': 50, 'Magic Resistance': 50}
         self.assertEqual(expected, create_character("Nathan"))
